23c0ed33a70b66c060aae9f46a2405bf6063b694
Stephen Silber <stephensilber@gmail.com>
2026-07-17 20:30:01 -0500

    Point Pulse dependency at ReshotLabs fork

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.2"),
-        .package(url: "https://github.com/kean/pulse.git", from: "5.0.0")
+        .package(url: "https://github.com/ReshotLabs/Pulse", branch: "main")
     ],
     targets: [
         .target(name: "PulseLogHandler", dependencies: [
